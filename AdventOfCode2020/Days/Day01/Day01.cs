68c7142efb6922841161e5b0052a2480cba9ed4b
Jurgen Coenegrachts <jurgen.coenegrachts@student.pxl.be>
2020-12-01 06:19:01 +0100

    Finish day 1

--- AdventOfCode2020/Days/Day01/Day01.cs
+++ AdventOfCode2020/Days/Day01/Day01.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2020.Days.Day01
 {
@@ -7,14 +9,42 @@ namespace AdventOfCode2020.Days.Day01
 		/// <inheritdoc />
 		public override string Part1()
 		{
-			Console.WriteLine("Day 1");
-			throw new System.NotImplementedException();
+			List<int> numbers = Input.Split('\n').Select(s => int.Parse(s)).ToList();
+
+			foreach (int number1 in numbers)
+			{
+				foreach (int number2 in numbers)
+				{
+					if (number1 + number2 == 2020)
+					{
+						return (number1 * number2).ToString();
+					}
+				}
+			}
+
+			return "none";
 		}
 
 		/// <inheritdoc />
 		public override string Part2()
 		{
-			throw new System.NotImplementedException();
+			List<int> numbers = Input.Split('\n').Select(s => int.Parse(s)).ToList();
+
+			foreach (int number1 in numbers)
+			{
+				foreach (int number2 in numbers)
+				{
+					foreach (int number3 in numbers)
+					{
+						if (number1 + number2 + number3 == 2020)
+						{
+							return (number1 * number2 * number3).ToString();
+						}
+					}
+				}
+			}
+
+			return "none";
 		}
 	}
 }
