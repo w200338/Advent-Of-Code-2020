192aed9700c106ba15c2399389df378d6d198836
Jurgen Coenegrachts <jurgen.coenegrachts@student.pxl.be>
2020-12-02 06:10:48 +0100

    Add day 2

--- AdventOfCode2020/Days/Day02/Day02.cs
+++ AdventOfCode2020/Days/Day02/Day02.cs
@@ -1,17 +1,57 @@
-﻿namespace AdventOfCode2020.Days.Day02
+﻿using System.Linq;
+
+namespace AdventOfCode2020.Days.Day02
 {
 	public class Day02 : Day
 	{
 		/// <inheritdoc />
 		public override string Part1()
 		{
-			return "not implemented";
+			string[] passwords = Input.Split('\n');
+
+			int valid = 0;
+			foreach (string password in passwords)
+			{
+				string[] parts = password.Split(new char[] {'-', ' ', ':'}, System.StringSplitOptions.RemoveEmptyEntries);
+
+				int min = int.Parse(parts[0]);
+				int max = int.Parse(parts[1]);
+				char character = parts[2][0];
+				string passwordAttempt = parts[3];
+
+				int countInPassword = passwordAttempt.Count(c => c == character);
+				if (countInPassword >= min && countInPassword <= max)
+				{
+					valid++;
+				}
+			}
+
+			return valid.ToString();
 		}
 
 		/// <inheritdoc />
 		public override string Part2()
 		{
-			return "not implemented";
+			string[] passwords = Input.Split('\n');
+
+			int valid = 0;
+			foreach (string password in passwords)
+			{
+				string[] parts = password.Split(new char[] { '-', ' ', ':' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+				int pos1 = int.Parse(parts[0]);
+				int pos2 = int.Parse(parts[1]);
+				char character = parts[2][0];
+				string passwordAttempt = parts[3];
+
+				if (passwordAttempt[pos1 - 1] == character && passwordAttempt[pos2 - 1] != character ||
+				    passwordAttempt[pos1 - 1] != character && passwordAttempt[pos2 - 1] == character)
+				{
+					valid++;
+				}
+			}
+
+			return valid.ToString();
 		}
 	}
 }
